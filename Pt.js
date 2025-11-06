a55b2d6180a7d419439b210cb7a7698d424566c1
Myles Dean <info@mylesdean.com>
2025-11-06 15:10:34 -0500

    Fix EventManager initialization in Pt.js

---
+++ Pt.js
@@ -0,0 +1,91 @@
+window.visualAssets = {
+    traditional: {
+        start: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a4899d44566581e7f0e1e_1%20Traditional.jpg',
+        bright: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a489978cbf3d00c78a86d_2%20Traditional%20Bright.jpg',
+        dimmed: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a4899d13315069b3c49cc_3%20Traditional%20Dimmed.jpg',
+        dark: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a4899ba3c78c0bf2677c5_4%20Traditional%20Dark.jpg',
+        color: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48999e586d95dd64343b_6%20Traditional%20Color.jpg',
+        white: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48990ead54d36d2572b8_5%20Traditional%20White.jpg',
+        calm: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a489957d83c08fa6b0dc0_7%20Traditional%20Calm.jpg',
+        noHaze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a489ec3931f2b0ec9c7d4_10%20Traditional%20No%20Haze.jpg',
+        haze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a489dc4e63ea3fd4e0dc5_11%20Traditional%20Haze.jpg',
+        subtle: 'https://vz-da639997-4dd.b-cdn.net/f3d205b3-6a64-4be6-b658-b85ba8e46ca0/playlist.m3u8',
+        dynamic: 'https://vz-da639997-4dd.b-cdn.net/b57f7ac1-9399-474c-8ac7-9c838f72ac78/playlist.m3u8'
+    },
+    blended: {
+        start: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a8a4828657843d821e_1%20Blended.jpg',
+        bright: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a8e0864a47dc7e80d3_2%20Blended%20Bright.jpg',
+        dimmed: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a8ab01c3c01e160d0e_3%20Blended%20Dimmed.jpg',
+        dark: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a809b96b072af5467b_4%20Blended%20Dark.jpg',
+        color: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a829c5fc58349562f5_5%20Blended%20Color.jpg',
+        white: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a834f802a4d876a041_6%20Blended%20White.jpg',
+        calm: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a8787d2d9aaeadc1cb_7%20Blended%20Calm.jpg',
+        noHaze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a8bfb6f8f37c2f9e7b_10%20Blended%20No%20Haze.jpg',
+        haze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48a8a6b2c46946074cb0_11%20Blended%20Haze.jpg',
+        subtle: 'https://vz-da639997-4dd.b-cdn.net/88c22b69-3dbd-436d-ba05-11793b5a390c/playlist.m3u8',
+        dynamic: 'https://vz-da639997-4dd.b-cdn.net/88c22b69-3dbd-436d-ba05-11793b5a390c/playlist.m3u8'
+    },
+    contemporary: {
+        start: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b2f09cf902ddb58f23_7%20Contemporary%20Calm.jpg',
+        bright: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b2ba3c78c0bf26812b_2%20Contemporary%20Bright.jpg',
+        dimmed: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b24af64e050c1a20d8_3%20Contemporary%20Dimmed.jpg',
+        dark: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b2f09cf902ddb58f23_7%20Contemporary%20Calm.jpg',
+        color: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b278cbf3d00c78addd_5%20Contemporary%20Color.jpg',
+        white: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b2f84b1615a7801c97_6%20Contemporary%20White.jpg',
+        calm: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b2f09cf902ddb58f23_7%20Contemporary%20Calm.jpg',
+        noHaze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b4ec80fa6254f103fe_10%20Contemporary%20No%20Haze.jpg',
+        haze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48b2f09cf902ddb58f23_7%20Contemporary%20Calm.jpg',
+        subtle: 'https://vz-da639997-4dd.b-cdn.net/4f909e06-69d2-47e0-b433-252bed006db3/playlist.m3u8',
+        dynamic: 'https://vz-da639997-4dd.b-cdn.net/46e26b17-61a0-4565-aa16-aa0e9a74b6d5/playlist.m3u8'
+    },
+    modern: {
+        start: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd491451044de1f53a_5%20Modern%20Color.jpg',
+        bright: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bdc344ee3cad11e582_2%20Modern%20Bright.jpg',
+        dimmed: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd7cab11bc2398c3ec_3%20Modern%20Dimmed.jpg',
+        dark: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd491451044de1f53a_5%20Modern%20Color.jpg',
+        color: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd491451044de1f53a_5%20Modern%20Color.jpg',
+        white: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd6243d284f5786eaa_6%20Modern%20White.jpg',
+        calm: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd491451044de1f53a_5%20Modern%20Color.jpg',
+        noHaze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bfd78db0eded90696e_10%20Modern%20No%20Haze.jpg',
+        haze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48bd491451044de1f53a_5%20Modern%20Color.jpg',
+        subtle: 'https://vz-da639997-4dd.b-cdn.net/707bd35a-5ee1-4830-8178-2100e1ca3aef/playlist.m3u8',
+        dynamic: 'https://vz-da639997-4dd.b-cdn.net/c14d7d5d-aa6c-41f8-98ee-90781ba4790c/playlist.m3u8'
+    },
+    acoustic: {
+        start: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c821e4809bd477aa7b_1%20Acoustic.jpg',
+        bright: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c8e3a0b51873d89462_2%20Acoustic%20Bright.jpg',
+        dimmed: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c757d83c08fa6b1c81_3%20Acoustic%20Dimmed.jpg',
+        dark: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c7f696c1d724fcf2f8_4%20Acoustic%20Dark.jpg',
+        color: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c712f4594662b091b6_5%20Acoustic%20Color.jpg',
+        white: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c821e4809bd477aa7b_1%20Acoustic.jpg',
+        calm: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c83fc2d81e4098e03a_7%20Acoustic%20Calm.jpg',
+        noHaze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c75d713f5973a31980_10%20Acoustic%20No%20Haze.jpg',
+        haze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48c821e4809bd477aa7b_1%20Acoustic.jpg',
+        subtle: 'https://vz-da639997-4dd.b-cdn.net/b216cfdd-1e54-4237-a0b7-80dc73c0f7db/playlist.m3u8',
+        dynamic: 'https://vz-da639997-4dd.b-cdn.net/b92ca9d1-453f-416f-9569-1b2c144b2271/playlist.m3u8'
+    },
+    youth: {
+        start: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d1a9e8de6cb2d9eccf_10%20Youth%20No%20Haze.jpg',
+        bright: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d40ebad338bc855210_2%20Youth%20Bright.jpg',
+        dimmed: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d42f9cc5fcc6949552_3%20Youth%20Dimmed.jpg',
+        dark: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d1a9e8de6cb2d9eccf_10%20Youth%20No%20Haze.jpg',
+        color: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d1a9e8de6cb2d9eccf_10%20Youth%20No%20Haze.jpg',
+        white: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d43387623f41f3a48e_6%20Youth%20White.jpg',
+        calm: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d1a9e8de6cb2d9eccf_10%20Youth%20No%20Haze.jpg',
+        noHaze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d1a9e8de6cb2d9eccf_10%20Youth%20No%20Haze.jpg',
+        haze: 'https://cdn.prod.website-files.com/68f66a5d60094384951f8504/690a48d18c874235fa389c81_11%20Youth%20Haze.jpg',
+        subtle: 'https://vz-da639997-4dd.b-cdn.net/ab0df18a-1443-4eda-aa27-9b3c9bc23166/playlist.m3u8',
+        dynamic: 'https://vz-da639997-4dd.b-cdn.net/44e29df9-8bc7-48fa-a157-7ad03785d124/playlist.m3u8'
+    }
+};
+
+// Make all classes global so they can be accessed across script blocks
+window.NavigationManager = null;
+window.VisualManager = null;
+window.DimensionManager = null;
+window.GoalManager = null;
+window.PhotoManager = null;
+window.DockManager = null;
+window.ValidationManager = null;
+window.StorageManager = null;
+window.EventManager = null;
